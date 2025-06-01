01e8c3663485b421205aef5a192f9cc732f16812
“matiascam02” <167996856+matiascam02@users.noreply.github.com>
2025-06-01 20:46:57 +0200

    fix start for railway

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 export PYTHONUNBUFFERED=1
-export PORT=${PORT:-5000}  # Railway define PORT automáticamente
-
-# Reemplaza con el script correcto si usas otro
+export PORT=${PORT:-5000}
 python run_ui.py
